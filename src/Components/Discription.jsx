fb7f0cdc3b63580cbb99be041f28d425f278d1d2
meetsoni9316 <meetsoni9316@gmail.com>
2024-07-10 21:30:05 +0530

    Update Discription.jsx

--- src/Components/Discription.jsx
+++ src/Components/Discription.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../Discription.css";
+
 
 function Discription() {
   return (
@@ -7,19 +9,19 @@ function Discription() {
       <div>
         <div className="md:mt-40 mt-32 md:ml-16 md:ml-10 ml-4  text-6xl font-bold z-0 moving-text">
           {" "}
-          <span className="bg-gradient-to-r from-blue-500 to-sb-8000 text-transparent bg-clip-text font-bold ">
+          <span className="bg-gradient-to-r from-blue-500 to-sb-8000 text-transparent bg-clip-text font-bold line line-1">
             {" "}
             Discover Delight in Every Bite
           </span>
         </div>
 
-        <div className="text-sb md:ml-10 ml-4 md:ml-16 mt-12  ">
-          Welcome to BistroBite! We're delighted to have you here to enjoy{" "}
-          <div>
-            our fresh, flavorful dishes crafted with passion. Relax in our{" "}
-          </div>
-          warm, inviting atmosphere and let us make your dining{" "}
-          <div>experience memorable. Bon appétit!</div>
+        <div className="text-sb md:ml-10 ml-4 md:ml-16 mt-12 text-xl md:w-1/2 line line-2  ">
+          Welcome to BistroBite! We're delighted to have you here to enjoy
+          
+            our fresh, flavorful dishes crafted with passion. Relax in our
+          
+          warm, inviting atmosphere and let us make your dining
+          experience memorable. Bon appétit!
         </div>
         <div className="mt-12 md:ml-16 md:ml-10 ml-4 font-bold text-4xl text-black ">
           {" "}
