b27c036f96a608707e03a8d3d4cb2bebfb1daa84
meetsoni9316 <meetsoni9316@gmail.com>
2024-07-11 21:47:02 +0530

    Update Reservation.jsx

--- src/Components/Reservation.jsx
+++ src/Components/Reservation.jsx
@@ -53,7 +53,7 @@ function Reservation() {
       <div className="bg-gradient-to-r from-mt to-ft overflow-hidden h-screen">
         <Navbar />
         <div className=" flex items-center justify-center md:mt-8 mt-20">
-          <div className="card md:w-96 md:h-105 w-80 h-96 glass">
+          <div className="card md:w-96 md:h-105 w-80 h-112 glass">
             <div className="card-body justify-center items-center ">
               <h2 className="card-title text-sb text-2xl font-bold ">
                 Reserve Your Spot
