5bba23135cc73cac9a513a2845f26d0de7fb9bb7
meetsoni9316 <meetsoni9316@gmail.com>
2024-07-11 22:04:46 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -50,7 +50,7 @@ function Login() {
       <div className="bg-gradient-to-r from-mt to-ft overflow-hidden h-screen">
         <Navbar />
         <div className=" flex items-center justify-center md:mt-28 mt-48">
-          <div className="card md:w-96 md:h-105 w-86 h-80 glass">
+          <div className="card md:w-96 md:h-105 w-92 h-96 glass">
             <div className="card-body justify-center items-center ">
               <h2 className="card-title text-sb text-2xl font-bold ">Login</h2>
               <form onSubmit={handleSubmit(onSubmit)}>
