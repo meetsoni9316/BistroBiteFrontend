ba1533f8fd39f047d53ddfaff9e26a7c426a5025
meetsoni9316 <meetsoni9316@gmail.com>
2024-07-11 21:20:47 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -50,7 +50,7 @@ function Login() {
       <div className="bg-gradient-to-r from-mt to-ft overflow-hidden h-screen">
         <Navbar />
         <div className=" flex items-center justify-center md:mt-28 mt-48">
-          <div className="card md:w-96 md:h-105 glass">
+          <div className="card md:w-96 md:h-105 w-80 h-96 glass">
             <div className="card-body justify-center items-center ">
               <h2 className="card-title text-sb text-2xl font-bold ">Login</h2>
               <form onSubmit={handleSubmit(onSubmit)}>
@@ -67,7 +67,7 @@ function Login() {
                   </svg>
                   <input
                     type="email"
-                    className="grow  w-64"
+                    className="grow md:w-64 w-36"
                     placeholder="Email"
                     {...register("email", { required: true })}
                   />
@@ -92,7 +92,7 @@ function Login() {
                   <input
                     type="password"
                     placeholder="Enter your password"
-                    className="w-64 grow  rounded-md outline-none "
+                    className="md:w-64 w-36 grow  rounded-md outline-none  "
                     {...register("password", { required: true })}
                   />
                 </label>
