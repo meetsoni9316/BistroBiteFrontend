c74812e83dc1f5c6784a80c38c60bc729d28e0ab
meetsoni9316 <meetsoni9316@gmail.com>
2024-07-11 21:19:01 +0530

    Update Feedback.jsx

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -50,7 +50,7 @@ function Feedback() {
       <div className="bg-gradient-to-r from-mt to-ft overflow-hidden h-screen">
         <Navbar />
         <div className=" flex items-center justify-center md:mt-24 mt-32">
-          <div className="card md:w-96 md:h-105 glass">
+          <div className="card md:w-96 md:h-105 w-80 h-96 glass">
             <div className="card-body justify-center items-center ">
               <h2 className="card-title text-sb text-2xl font-bold ">
                 Feedback
@@ -69,7 +69,7 @@ function Feedback() {
                   </svg>
                   <input
                     type="text"
-                    className="grow w-64"
+                    className="grow md:w-64 w-36"
                     placeholder="Username"
                     {...register("username", { required: true })}
                   />
@@ -93,7 +93,7 @@ function Feedback() {
                   </svg>
                   <input
                     type="email"
-                    className="grow w-64"
+                    className="grow md:w-64 w-36"
                     placeholder="Email"
                     {...register("email", { required: true })}
                   />
@@ -120,7 +120,7 @@ function Feedback() {
                   <input
                     type="text"
                     placeholder="Write your message here"
-                    className="w-64 px-3 grow "
+                    className="w-64 px-3 md:w-64 w-36 "
                     {...register("message", { required: true })}
                   />
                 </label>
